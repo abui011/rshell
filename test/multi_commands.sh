cacff8cf6710f143ef5b8466abf6ea821f9e66fe
abui011 <abui011@ucr.edu>
2016-05-16 22:22:03 -0700

    Create multi_commands.sh

---
+++ test/multi_commands.sh
@@ -0,0 +1,21 @@
+echo Testing multiple commands
+
+
+echo A; echo B; echo C;
+mkdir newfolder && echo B
+mkdir newfolder && echo This shouldn't echo
+mkdir newfolder || echo This should
+echo A && echo B && echo C
+echo A && echo B || echo This shouldn't echo
+echo A || echo B && echo C
+echo A; echo B && echo C
+echo A; echo B || echo C
+ls; ls; ls;
+ls || ls || ls
+echo A        #;echo B
+echo ls ;ls
+echo A            && echo B
+echo A   &&    echo B
+
+echo All multiple commands tested
+echo Testing completed
