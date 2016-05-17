771577ee5d8861ae6a5d17159ddd8e228feb15b5
abui011 <abui011@ucr.edu>
2016-05-16 22:21:16 -0700

    Create commented_command.sh

---
+++ test/commented_command.sh
@@ -0,0 +1,9 @@
+#tests commands with comments
+
+
+echo Checking for comments #ls -a
+echo Comments work
+ls -a #echo this should not display
+echo "#this comment should work though"
+echo "this one on the right shouldn't work though" #this one right here
+exit
