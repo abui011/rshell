a8e9b3aa71b72879772ad1007d6e3cc08bdded84
abui011 <abui011@ucr.edu>
2016-05-30 23:55:08 -0700

    Create precedence_test.sh

---
+++ test/precedence_test.sh
@@ -0,0 +1,12 @@
+(echo a && echo b) && echo b
+echo a && (echo a && echo b) && echo b
+mkdir folder
+(mkdir folder && echo b) && echo a
+(mkdir folder && echo b) || echo a
+(     )
+(echo a) && (adsada)
+(adasda)
+echo (a && b)
+(echo a)
+(echo a && echo b)
+(echo a || echo b)
